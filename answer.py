7f065fa910ce025bb000730479434c2b13497ded
belousovromnik <belousov.romnik@yandex.ru>
2020-01-08 19:04:30 +0300

    вариант 2

--- answer.py
+++ answer.py
@@ -13,6 +13,8 @@ def answer():
 
 
 def answer_api(cnt = 1):
+    import json
+
     itog_arr = {}
     if cnt == 1:
         itog_arr = {"message": answer()}
@@ -22,6 +24,6 @@ def answer_api(cnt = 1):
             strt.append(answer())
         itog_arr = {"messages": strt}
 
-    return itog_arr
+    return json.dumps(itog_arr)
 
-# print(answer_api(5))
+print(answer_api(5))
